f1d5cddf9ef56292630ecc331114dcc1f6a5a599
João Santos <joaomarcelo.ms@gmail.com>
2024-08-20 17:37:11 -0300

    renaming title

--- app/src/main/java/com/jomar/poc/quickactionsbrq/ShortcutTypeEnum.kt
+++ app/src/main/java/com/jomar/poc/quickactionsbrq/ShortcutTypeEnum.kt
@@ -1,7 +1,7 @@
 package com.jomar.poc.quickactionsbrq
 
 enum class ShortcutTypeEnum(val type: String, val description: String, val label: String) {
-    STATIC("Static","Atalho estático foi clicado","Adicionar QuickAction Estática"),
-    DYNAMIC("Dynamic","Atalho dinâmico foi clicado","Adicionar QuickAction Dinamica"),
-    PINNED("Pinned","Atalho pinado foi clicado","Adicionar QuickAction Pinned")
+    STATIC("Static","Atalho estático foi clicado","Adicionar Shortcut Estática"),
+    DYNAMIC("Dynamic","Atalho dinâmico foi clicado","Adicionar Shortcut Dinamica"),
+    PINNED("Pinned","Atalho pinado foi clicado","Adicionar Shortcut Pinned")
 }
